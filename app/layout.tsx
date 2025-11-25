21dbea2ecc5ecbd28f65e633dfcd1b8250e733c5
hcsn0459 <hcsn0459@gmail.com>
2025-11-25 15:11:46 +0800

    chore: add suppressHydrationWarning property

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <AuthProvider>
